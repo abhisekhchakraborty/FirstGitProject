efb0e3c24dde257c598941b80d8b4ace390877da
abhisekh <abhisekhchakraborty@gmail.com>
2022-08-24 18:06:33 +0530

    added to validate branching

--- Greeting.java
+++ Greeting.java
@@ -2,5 +2,7 @@ class Greeting{
     public static void main(String[] args){
         system.out.println("Greetings !!!");
         system.out.println("my friend !!!");
+
+        system.out.println("a new line added to validate branching");
     }
 }
